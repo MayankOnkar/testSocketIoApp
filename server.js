c09a59360967730624ce8173e987a0c4328de9b1
unknown <mayankonkar97@gmail.com>
2023-06-06 13:54:18 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    console.log('Message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log('Server running on port ' + port);
+});
